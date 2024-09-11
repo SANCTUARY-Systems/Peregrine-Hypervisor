f052a5f96ecc5f1be7d254f5bfe12521c0a39dcf
Radu Mantu <andru.mantu@gmail.com>
2024-09-11 15:14:31 +0300

    Added Sanctuary license to new sources
    These are not covered under the BSD 3-Clause license since they were not
    part of the original project.

--- src/arch/aarch64/virtioac.c
+++ src/arch/aarch64/virtioac.c
@@ -1,12 +1,26 @@
+/*
+ * Copyright (c) 2023 SANCTUARY Systems GmbH
+ *
+ * This file is free software: you may copy, redistribute and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For a commercial license, please contact SANCTUARY Systems GmbH
+ * directly at info@sanctuary.dev
+ */
+
 #include "pg/arch/virtioac.h"
 #include "pg/dlog.h"
 #include "pg/panic.h"
-
-
-
-
-
-
 
 static void writereg(struct vcpu *vcpu, uint8_t srt, uint8_t size, uint64_t data) {
 	if        (size == 0) {
